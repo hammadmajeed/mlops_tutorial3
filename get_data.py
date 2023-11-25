a7ac28d40a5391e8ea4feea256e1fdb0a46d7e5b
Hammad Majeed <hammadmajeed@gmail.com>
2023-11-25 08:49:50 +0500

    dvc pipeline

--- get_data.py
+++ get_data.py
@@ -1,18 +1,11 @@
-import os
-import wget
 
 # data from https://www.sciencedirect.com/science/article/pii/S2352340920303048
 
 # Download the zipped dataset
-url = 'https://md-datasets-cache-zipfiles-prod.s3.eu-west-1.amazonaws.com/yshdbyj6zy-1.zip'
-zip_name = "data.zip"
-wget.download(url, zip_name)
+url = 'https://drive.google.com/file/d/1NFjX2SUFheYRACqiwC6Vno7Fh-mmYna5/view?usp=sharing'
+name = "data_raw.csv"
 
-# Unzip it and standardize the .csv filename
-import zipfile
-with zipfile.ZipFile(zip_name,"r") as zip_ref:
-    zip_ref.filelist[0].filename = 'data_raw.csv'
-    zip_ref.extract(zip_ref.filelist[0])
+import gdown
 
-os.remove(zip_name)
+gdown.download(url, name, quiet=False, fuzzy=True)
 
