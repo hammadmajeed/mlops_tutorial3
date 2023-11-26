b7767c88edc955adbf1f336f79668ce13d07a657
Hammad Majeed <hammadmajeed@gmail.com>
2023-11-26 07:57:28 +0500

    updates

--- train.py
+++ train.py
@@ -11,7 +11,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.impute import SimpleImputer
 
-#added to satisfy Raheeimeen
 df = pd.read_csv("data_processed.csv")
 
 #### Get features ready to model! 
